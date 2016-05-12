5419ffb61529ac27ffa426a2354bfc2b18f095ae
g14955ws <g14955ws@s20620.tsuda.ac.jp>
2016-05-12 20:10:53 +0900

    課題4-4 エラー修正

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -1,11 +1,13 @@
 package snakecamel;
 
+import java.util.ArrayList;
+
 public class SnakeCamelUtil {
 
 	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+		String[] words = snake_case.split("_+");//_の前までのwordを取得	
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -16,13 +18,15 @@ public class SnakeCamelUtil {
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
 			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
+			if (Character.isUpperCase(c)) {//大文字だったら
+				sb.append(camelcase.substring(j, i));//jからi-1番目までを切り取り
+			
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
+	
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
@@ -30,15 +34,17 @@ public class SnakeCamelUtil {
 	}
 	
 	static String capitalize(String s) {
+		if (s.equals("")) return "";
 		char first = s.charAt(0);
-		char upperFirst = Character.toUpperCase(first);
+		char upperFirst = Character.toUpperCase(first);//toUpperCaseで大文字に変換
 		String rest = s.substring(1);
 		return upperFirst + rest;
 	}
 
 	static String uncapitalize(String s) {
+		if (s.equals("")) return "";
 		char first = s.charAt(0);
-		char lowerFirst = Character.toLowerCase(first);
+		char lowerFirst = Character.toLowerCase(first);//小文字に変換
 		String rest = s.substring(1);
 		return lowerFirst + rest;
 	}
--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,44 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class SnakeCamelTest {
 
+	/*
 	@Test
+	
 	public final void test() {
 		fail("Not yet implemented");
-	}
+	}*/
 
+	@Test
+	public void snaketocamelでapple_orangeの大文字変換ができる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String exp = "AppleOrange";
+		String actual = scu.snakeToCamelcase("apple_orange");
+		assertThat(actual,is(exp));
+	}
+	
+	@Test
+	public void camelToSnakeでAppleOrangeの小文字変換ができる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String exp = "apple_orange";
+		String actual = scu.camelToSnakecase("AppleOrange");
+		assertThat(actual,is(exp));
+	}
+	
+	@Test
+	public void cameltosnakeでacatの大文字変換ができる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String exp = "ACat";
+		String actual = scu.snakeToCamelcase("a_cat");
+		assertThat(actual,is(exp));
+	}
+	
+	
+	
+	
 }
